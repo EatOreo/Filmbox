c19db57ade9edd56a2f5225345786e0c9a9a93c8
luhj <luhj@itu.dk>
2024-01-18 17:21:52 +0100

    vscode livereload and htmx

---
+++ filmbox/src/main/java/com/eatoreo/filmbox/IndexController.java
@@ -0,0 +1,23 @@
+package com.eatoreo.filmbox;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public ModelAndView Index() {
+        var m = new ModelAndView("index");
+        return m;
+    }
+
+    @GetMapping("/api/sample/{name}")
+    public ModelAndView getSamplePage(@PathVariable String name) {
+        var m = new ModelAndView("sample");
+        m.addObject("name", name);
+        return m;
+    }
+}
