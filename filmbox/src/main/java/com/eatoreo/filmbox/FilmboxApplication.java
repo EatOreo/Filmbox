9ba5b97caa8ef344f98653af5c649b8b42430d94
luhj <luhj@itu.dk>
2024-01-20 15:35:18 +0100

    add layout and pico.css

--- filmbox/src/main/java/com/eatoreo/filmbox/FilmboxApplication.java
+++ filmbox/src/main/java/com/eatoreo/filmbox/FilmboxApplication.java
@@ -2,11 +2,20 @@ package com.eatoreo.filmbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 
 @SpringBootApplication
 public class FilmboxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilmboxApplication.class, args);
+	}
+
+	@Bean
+	public LayoutDialect layoutDialect() {
+		return new LayoutDialect();
 	}
 }
