0a0f8429762fb1a63e966488abecc7be3169e343
luhj <luhj@itu.dk>
2024-01-26 18:21:09 +0100

    use GetFilms.py in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,15 @@
+FROM python:3 AS films
+WORKDIR /drive
+COPY ["scripts/GetFilms.py", "GetFilms.py"]
+COPY ["filmbox/src/main/resources/static/films", "filmbox/src/main/resources/static/films"]
+RUN pip install checksumdir
+RUN pip install gdown
+RUN python GetFilms.py
+
 FROM eclipse-temurin:21-jdk AS build
 WORKDIR /src
 COPY ["filmbox/", "filmbox/"]
+COPY --from=films /drive/filmbox/src/main/resources/static/films filmbox/src/main/resources/static/films
 WORKDIR "/src/filmbox"
 RUN ./mvnw install
 
--- scripts/GetFilms.py
+++ scripts/GetFilms.py
@@ -3,12 +3,13 @@ from checksumdir import dirhash
 import shutil
 import os
 
+staticpath = './filmbox/src/main/resources/static'
 md5remote = "a96fb3a15aa2e9b8e84c6fbe44c5ee2f"
-md5hash = dirhash('./filmbox/src/main/resources/static/films', 'md5')
+md5hash = dirhash(staticpath + '/films', 'md5')
 url = "https://drive.google.com/drive/folders/1k5xysbb2GkzPil970SSmCxl8CRJ1ljsD"
 
 if md5hash != md5remote:
     gdown.download_folder(url, quiet=False, use_cookies=False)
-    if os.path.isdir('./filmbox/src/main/resources/static/films'):
-        shutil.rmtree('./filmbox/src/main/resources/static/films')
-    shutil.move('./films', './filmbox/src/main/resources/static')
+    if os.path.isdir(staticpath + '/films'):
+        shutil.rmtree(staticpath + '/films')
+    shutil.move('./films', staticpath)
